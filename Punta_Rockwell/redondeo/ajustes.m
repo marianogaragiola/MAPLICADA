4fe08fc3898851c174c90416cb222630b13af723
marianogaragiola <marianogaragiola@gmail.com>
2015-06-15 11:13:32 -0300

    lo nuevo

--- Punta_Rockwell/redondeo/ajustes.m
+++ Punta_Rockwell/redondeo/ajustes.m
@@ -12,52 +12,48 @@ NUM_PUNTOS = 320;
 % NOTA PARA ELEGIR EL NUMERO DE DECIMALES:
 % 4 decimales seria redondear a la decima de micrometro.
 % 5 decimales seria redondear a la centena de micrometro.
-decimales = 3;
+% decimales = 3;
 format long;
 r_medio = [] ;
 ang_medio = [] ;
 theta_medio = [] ;
 
+[r, rsim] = punta3(L, R, R2, alpha, sigma, NUM_PUNTOS);
+
+resultado = [];
+
 for decimales = 1: 7
-  radio = [];
-  angulo = [];
-  theta = [];
 
-  for i = 1: 10
+  rsim2 = redondeo(rsim, decimales);
 
-    [r, rsim] = punta3(L, R, R2, alpha, sigma, NUM_PUNTOS);
+  [popt, d, dist, theta_umbral] = cuad_ort_punta(L, R, R2, alpha, sigma, rsim2);
 
-    rsim = redondeo(rsim, decimales);
+  resultado = [resultado; decimales popt(6) popt(7) popt(1)];
 
-    [popt, d, dist, theta_umbral] = cuad_ort_punta(L, R, R2, alpha, sigma, rsim);
-
-    radio = [radio; popt(6)];
-    angulo = [angulo; popt(7)];
-    theta = [theta; popt(1)];
-  end
-
-  r_medio = [ r_medio; decimales mean(radio) std(radio)] ;
-  ang_medio = [ ang_medio; decimales mean(angulo) std(angulo)] ;
-  theta_medio = [ theta_medio; decimales mean(theta) std(theta)] ;
-% aca hago el ajuste con los datos medidos por clemar
 end
+save('redondeo.dat', '-ascii', 'resultado')
+figure
+plot(resultado(:,1),resultado(:,2))
+figure
+plot(resultado(:,1),resultado(:,3))
+figure
+plot(resultado(:,1),resultado(:,4))
 
-% filename='Datos_XYZ_Punta_SN_5620-R2=025mm.TXT';
-%
-% rmed = load(filename, '-ascii');
-% rmed = redondeo(rmed, decimales);
-%
-% [popt, d, dist, theta_umbral] = cuad_ort_punta(L, R, R2, alpha, sigma, rmed);
-%
-% theta_angulo_solido = acos(rmed(:,3)./sqrt(rmed(:,1).^2+rmed(:,2).^2+rmed(:,3).^2));
-% phi_angulo_solido = (1-sign(rmed(:,2)))/2*pi + acos(rmed(:,1)./sqrt(rmed(:,1).^2+rmed(:,2).^2));
-% dist = [phi_angulo_solido theta_angulo_solido dist];
-%
-% % desviacion estandar con los datos medidos
-% sigma_med = std(dist(:,6));
-%
-% save('-ascii', 'distancias.dat', 'dist');
-%
-% r_medido = [popt(6) std(radio)] ;
-% angulo_medido = [popt(7) std(angulo)] ;
-% theta_medido = [popt(1) std(angulo)] ;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+for i = 1:20
+  [r, rsim] = punta3(L, R, R2, alpha, sigma, NUM_PUNTOS);
+
+  resultado = [];
+
+  for decimales = 1: 7
+
+    rsim2 = redondeo(rsim, decimales);
+
+    [popt, d, dist, theta_umbral] = cuad_ort_punta(L, R, R2, alpha, sigma, rsim2);
+
+    resultado = [resultado; decimales popt(6) popt(7) popt(1)];
+
+  endfor
+endfor
