5f3810c7842af24f37549b4abf1f1f4ddb4648df
marianogaragiola <marianogaragiola@gmail.com>
2015-09-24 12:00:03 -0300

    algunas funciones que nos olvidamos en el commit anterior

--- Punta_Rockwell/src/ajustes_parametros.m
+++ Punta_Rockwell/src/ajustes_parametros.m
@@ -40,7 +40,7 @@ save('-ascii', '../salida/distancia.txt', 'dist');
 sigma_med = std(dist(:,4));
 sigma = sigma_med;
 
-<<<<<<< HEAD
+
 % Ahora creamos los datos simulados y calcular el error en los parametros.
 
 radio = [];
@@ -73,38 +73,3 @@ fprintf(file, 'Angulo entre el eje z y el eje del cono, error monte carlo = (%.1
 fprintf(file, 'Angulo entre el eje z y el eje del cono, error matriz covarianza = (%.12e +/- %.12e) [min]\n', popt_err(1,1), popt_err(1,2));
 
 fclose(file);
-=======
-
-% Ahora creamos los datos simulados y calcular el error en los parametros.
-
-% radio = [];
-% angulo = [];
-% theta = [];
-%
-% for i = 1: Nsim
-%   [r, rsim] = punta(L, R, R2, alpha, sigma, NUM_PUNTOS);
-%
-%   [popt2, d2, dist2, theta_umbral2] = cuad_ort_punta(L, R, R2, alpha, sigma, rsim);
-%
-%   radio = [radio; popt2(6)];
-%   angulo = [angulo; popt2(7)];
-%   theta = [theta; popt2(1)];
-% end
-%
-% r_medido = [popt(6) std(radio)] ;
-% angulo_medido = [popt(7) std(angulo)] ;
-% theta_medido = [popt(1) std(theta)] ;
-%
-% file = fopen('../salida/parametros_optimizacion.txt', 'w');
-%
-% fprintf(file, 'Radio de la esfera palpadora, error monte carlo = (%.12e +/- %.12e) [milimetros] \n', r_medido(1), r_medido(2));
-% fprintf(file, 'Radio de la esfera palpadora, error matriz covarianza = (%.12e +/- %.12e) [milimetros] \n\n', popt_err(6,1), popt_err(6,2));
-%
-% fprintf(file, 'Angulo de abertura del cono, error monte carlo = (%.12e +/- %.12e) [grados]\n', angulo_medido(1), angulo_medido(2));
-% fprintf(file, 'Angulo de abertura del cono, error matriz covarianza = (%.12e +/- %.12e) [grados]\n\n', popt_err(7,1), popt_err(7,2));
-%
-% fprintf(file, 'Angulo entre el eje z y el eje del cono, error monte carlo = (%.12e +/- %.12e) [min]\n', theta_medido(1), theta_medido(2));
-% fprintf(file, 'Angulo entre el eje z y el eje del cono, error matriz covarianza = (%.12e +/- %.12e) [min]\n', popt_err(1,1), popt_err(1,2));
-%
-% fclose(file);
->>>>>>> de6c8235d6189b5a1a38f835a1cf52e1954f0e2f
--- Punta_Rockwell/src/fun.m
+++ Punta_Rockwell/src/fun.m
@@ -4,9 +4,25 @@
 
 % esta funcion no usa pesos estadisticos sobre los puntos de la esfera y
 % del cono.
-function y = fun( p, X, I)
+function y = fun(X, p);
 
-y = [distesf(p, X(I,:)); distcono(p, X(~I,:)) ] ;
-y = norm(y);
+p = p(:)';
+
+V0(1:3) = p(3:5);
+v1 = [cos(p(2))*sin(p(1)), sin(p(2))*sin(p(1)), cos(p(1))];
+V1 = repmat(v1, [size(X,1), 1]);
+
+c = V0(1:3) + p(6)*v1(1:3)/sin(p(7));
+C = repmat(c, [size(X,1), 1]);
+
+II = ( -sin(p(7)) >= dot(X-C,V1,2)./sqrt(dot(X-C,X-C,2)) ) ;
+
+X_esf = X(II,:);
+X_cono = X(~II,:);
+
+y_esf = distesf(X_esf, p);
+y_cono = distcono(X_cono, p);
+
+y = [y_esf; y_cono ] ;
 
 end
